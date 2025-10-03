f347c0865ba105a09036068f76d652ace4695f3a
Johnwesley17 <148932028+Johnwesley17@users.noreply.github.com>
2025-10-03 12:18:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import time
 from news_sender import send_email
 
 # Schedule: Every day at 08:00 AM
-schedule.every().day.at("11:25").do(send_email)
+schedule.every().day.at("12:21").do(send_email)
 
 print("📆 DailyByte Email Scheduler Started...")
 
 while True:
     schedule.run_pending()
     time.sleep(60)
+
